b922e2e16610f5fcf81c2c88c5cba2466fb274bd
marinamjk <marinamjk@gmail.com>
2024-10-20 05:55:10 -0300

    basededatos

--- TP-Cuatrimestral-Equipo-15B/DataBase_Equipo15B.sql
+++ TP-Cuatrimestral-Equipo-15B/DataBase_Equipo15B.sql
@@ -3,34 +3,32 @@ Collate Latin1_General_CI_AI
 Go
 Use ECOMMERCE_EQUIPO15B
 
-Create Table Direccion(
-IDDireccion int not null primary key,
-Calle varchar(100) not null,
-Altura int null,
-Ciudad int not null,
-CodigoPostal int not null,
-Provincia varchar(50) not null
-)
-
 Create Table Usuarios(
-IDUsuario int not null primary key,
+IDUsuario int not null primary key identity(1,1),
+IDTipoUsuario int not null,
 Nombre varchar(50) not null,
 Apellido varchar(50) not null,
 DNI varchar(10) not null,
 Email varchar(250) not null,
 Contrasenia varchar(50) not null,
 Telefono varchar(50) null,
-IDDireccion int null foreign key references Direccion(IDDireccion),
+IDDireccion int null,
 IDTarjeta int null,
 UrlFotoPerfil varchar(500) null
 )
 
 Create Table Articulos(
-IDArticulo int not null primary key,
+IDArticulo int not null primary key identity(1,1),
 Nombre varchar(100) not null,
 Descripcion varchar(3000) null,
 Precio money not null,
 IDCategoria int null,
 IDMarca int null,
 Stock int not null
+)
+
+Create Table Imagenes(
+IDImagen int not null primary key identity(1,1),
+IDArticulo int not null foreign key references Articulos(IDArticulo),
+UrlImagen varchar(500) not null
 )
